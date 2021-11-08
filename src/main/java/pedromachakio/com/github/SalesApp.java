6c74f047734adba1397cf6113a843ed4078e00f9
Pedro Batista <pedro.batista@bringglobal.com>
2021-11-08 18:03:09 -0100

    first commit with empty project but with updated dependencies and .properties file

---
+++ src/main/java/pedromachakio/com/github/SalesApp.java
@@ -0,0 +1,13 @@
+package pedromachakio.com.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesApp.class, args);
+
+    }
+}
