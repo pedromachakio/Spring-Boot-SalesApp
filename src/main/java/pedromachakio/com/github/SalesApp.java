bb76373d1fba60a823d2e9904ca97c081c10c6ce
pedromachakio <pedromachakio@hotmail.com>
2021-11-10 18:48:52 -0100

    refactored the other CRUD methods to use JPA instead of jdbc (but left it commented)

--- src/main/java/pedromachakio/com/github/SalesApp.java
+++ src/main/java/pedromachakio/com/github/SalesApp.java
@@ -24,17 +24,18 @@ public class SalesApp {
             List<Client> listOfClients = clientsDAO.displayClientsList();
             listOfClients.forEach(System.out::println);
 
-            /*System.out.println("--- Updating Name Of Clients ---");
+            System.out.println("--- Updating Name Of Clients ---");
             listOfClients.forEach(client -> {
                 client.setName(client.getName() + " ganda atualização");
                 clientsDAO.update(client);
             });
 
+
             System.out.println("--- Getting Client by Specific Name, Including Partial Match ---");
             clientsDAO.getClientByName("Heal").forEach(System.out::println);
 
 
-            //clientsDAO.displayClientsList().forEach(clientsDAO::delete);
+            clientsDAO.displayClientsList().forEach(clientsDAO::delete);
 
             listOfClients = clientsDAO.displayClientsList(); // buscar novamente a lista de clientes para atualizar aqui com o que está no h2
              System.out.println("--- Displaying List of Clients After Delete ---");
@@ -42,7 +43,7 @@ public class SalesApp {
                 System.out.println("No clients left.");
             } else {
                 listOfClients.forEach(System.out::println);
-            }*/
+            }
         };
     }
 
--- src/main/java/pedromachakio/com/github/domain/repository/ClientsDAO.java
+++ src/main/java/pedromachakio/com/github/domain/repository/ClientsDAO.java
@@ -2,26 +2,26 @@ package pedromachakio.com.github.domain.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import pedromachakio.com.github.domain.entity.Client;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
 public class ClientsDAO {
 
     //private static final String SQL_INSERT = "INSERT INTO CLIENT (NAME) VALUES (?) ";
-    private static final String SQL_SELECT_ALL = "SELECT * FROM CLIENT ";
-    private static final String SQL_SELECT_SPECIFIC_CLIENT = "SELECT * FROM CLIENT WHERE NAME LIKE ? ";
-    private static final String SQL_UPDATE = "UPDATE CLIENT SET NAME = ? WHERE ID = ? ";
-    private static final String SQL_DELETE = "DELETE FROM CLIENT WHERE ID = ? ";
+    //private static final String SQL_SELECT_ALL = "SELECT * FROM CLIENT ";
+    //private static final String SQL_SELECT_SPECIFIC_CLIENT = "SELECT * FROM CLIENT WHERE NAME LIKE ? ";
+    //private static final String SQL_UPDATE = "UPDATE CLIENT SET NAME = ? WHERE ID = ? ";
+    //private static final String SQL_DELETE = "DELETE FROM CLIENT WHERE ID = ? ";
 
 
-    @Autowired // para o Spring injetar uma instância de jdbcTemplate para poder usá-lo
-    JdbcTemplate jdbcTemplate;
+    /*@Autowired // para o Spring injetar uma instância de jdbcTemplate para poder usá-lo
+    JdbcTemplate jdbcTemplate;*/
 
     @Autowired
     EntityManager entityManager; // responsible for all operations with the mapped entities
@@ -33,34 +33,54 @@ public class ClientsDAO {
         return client;
     }
 
+    @Transactional
     public Client update(Client client) {
-        jdbcTemplate.update(SQL_UPDATE, client.getName(), client.getId()); // removed explicit array creation as above
+        //jdbcTemplate.update(SQL_UPDATE, client.getName(), client.getId()); // removed explicit array creation as above
+        entityManager.merge(client); // sincroniza este cliente com o cliente que foi anteriormente salvo no entity manager
         return client;
     }
 
+    @Transactional
     public void delete(Client client) {
-        delete(client.getId()); // assim os dois métodos que fazem a mesma coisa convergem só num independentemente de o utilizador fornecer um Cliente ou apenas um Id
+        //delete(client.getId()); // assim os dois métodos que fazem a mesma coisa convergem só num independentemente de o utilizador fornecer um Cliente ou apenas um Id
+        if (!entityManager.contains(client)) {
+            client = entityManager.merge(client);
+        }
+        entityManager.remove(client);
     }
 
+    @Transactional
     public void delete(Integer id) {
-        jdbcTemplate.update(SQL_DELETE, id);
+        //jdbcTemplate.update(SQL_DELETE, id);
+
+        Client clientToBeDeleted = entityManager.find(Client.class, id);
+        delete(clientToBeDeleted);
+
+        // podia simplificar as such delete(entityManager.find(Client.class, id));
     }
 
+    @Transactional(readOnly = true)
     public List<Client> getClientByName(String name) {
-        return jdbcTemplate.query(SQL_SELECT_SPECIFIC_CLIENT, new Object[]{"%" + name + "%"}, getRowMapper());
+        //return jdbcTemplate.query(SQL_SELECT_SPECIFIC_CLIENT, new Object[]{"%" + name + "%"}, getRowMapper());
+        String jpql = " select c from Client c where c.name like :name "; // os dois pontos indicam que é um parâmetro que vai ser passado
+        TypedQuery<Client> query = entityManager.createQuery(jpql, Client.class);
+        query.setParameter("name", "%" + name + "%");
+        return query.getResultList();
     }
 
+    @Transactional(readOnly = true)
     public List<Client> displayClientsList() {
-        return jdbcTemplate.query(SQL_SELECT_ALL, getRowMapper());
+        //return jdbcTemplate.query(SQL_SELECT_ALL, getRowMapper());
+        return entityManager.createQuery("from Client", Client.class).getResultList();
     }
 
-    private RowMapper<Client> getRowMapper() {
+    /*private RowMapper<Client> getRowMapper() {
         return (resultSet, i) -> {
             Integer id = resultSet.getInt("ID");
             String nome = resultSet.getString("NAME");
             return new Client(id, nome);
         };
-    }
+    }*/
 
 
 }
