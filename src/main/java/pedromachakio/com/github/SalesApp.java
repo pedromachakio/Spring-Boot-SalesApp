7d9cc2d524790fea27cfb1846f5be888df5354a8
pedromachakio <pedromachakio@hotmail.com>
2021-11-10 18:48:52 -0100

    creating custom query methods with the @Query / @Param and @Modifying annotations

--- src/main/java/pedromachakio/com/github/SalesApp.java
+++ src/main/java/pedromachakio/com/github/SalesApp.java
@@ -20,33 +20,8 @@ public class SalesApp {
             clientsDAO.save(new Client("Pedriem"));
             clientsDAO.save(new Client("BeThereHealth"));
 
-            //stem.out.println("--- Displaying List of Clients ---");
-            //List<Client> listOfClients = clientsDAO.findAll();
-            //listOfClients.forEach(System.out::println);
-            boolean clientExists = clientsDAO.existsByName("Pedriem");
-            System.out.println("Is there a client with the name Pedriem? " + clientExists);
-
-
-            /*System.out.println("--- Updating Name Of Clients ---");
-            listOfClients.forEach(client -> {
-                client.setName(client.getName() + " ganda atualização");
-                clientsDAO.save(client); // save do jpa repository tanto guarda como dá update, por isso é usado para ambos
-            });
-
-
-            System.out.println("--- Getting Client by Specific Name, Including Partial Match ---");
-            clientsDAO.findByNameLike("Heal").forEach(System.out::println);
-
-
-            clientsDAO.findAll().forEach(clientsDAO::delete);
-
-            listOfClients = clientsDAO.findAll(); // buscar novamente a lista de clientes para atualizar aqui com o que está no h2
-             System.out.println("--- Displaying List of Clients After Delete ---");
-            if (listOfClients.isEmpty()) {
-                System.out.println("No clients left.");
-            } else {
-                listOfClients.forEach(System.out::println);
-            }*/
+           List<Client> clientsWithMatchingName = clientsDAO.findClientWithPartiallyMatchingName("Heal");
+           clientsWithMatchingName.forEach(System.out::println);
         };
     }
 
--- src/main/java/pedromachakio/com/github/domain/repository/ClientsDAO.java
+++ src/main/java/pedromachakio/com/github/domain/repository/ClientsDAO.java
@@ -1,13 +1,25 @@
 package pedromachakio.com.github.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pedromachakio.com.github.domain.entity.Client;
 
 import java.util.List;
 
 public interface ClientsDAO extends JpaRepository<Client, Integer> {
 
-    List<Client> findByNameLike(String name); // has to follow convenção query methods e fica ready behind the scenes
+    List<Client> findByNameLike(String name); // has to follow convenção dos query methods e fica ready behind the scenes
 
-    boolean existsByName (String name);
+    //se não for para seguir a convenção, pode-se usar qualquer nome desde que se use a annotation @Query e a query
+    // assim pode-se fazer pesquisas mais complexas. P.s. dá para usar native SQL tmb, such as:
+    //@Query(value = " select * from cliente c where c.nome like '%:nome%' ", nativeQuery = true)
+    @Query(value = " select c from Client c where c.name like :name ")
+    List<Client> findClientWithPartiallyMatchingName(@Param("name") String name);
+
+    boolean existsByName(String name);
+
+    @Modifying // os métodos que não são apenas de consulta e que modificam as tabelas têm de ser anotados com esta
+    void deleteByName(String name);
 }
