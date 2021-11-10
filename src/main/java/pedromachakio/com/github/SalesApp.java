01ab87a6d7338c744d7739e66b59cbe61b37c998
pedromachakio <pedromachakio@hotmail.com>
2021-11-10 18:48:51 -0100

    created the Clients DAO with save and retrieve client / list of clients using JdbcTemplate

--- src/main/java/pedromachakio/com/github/SalesApp.java
+++ src/main/java/pedromachakio/com/github/SalesApp.java
@@ -1,10 +1,33 @@
 package pedromachakio.com.github;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import pedromachakio.com.github.domain.entity.Client;
+import pedromachakio.com.github.domain.repository.Clients;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SalesApp {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clients clients) {
+        return args -> {
+            Client testClient = new Client();
+            testClient.setName("Pedriem");
+            clients.saveClient(testClient);
+
+            Client testClient2 = new Client();
+            testClient2.setName("BeThereHealth");
+            clients.saveClient(testClient2);
+
+            List<Client> clientsList = clients.displayClientsList();
+            clientsList.forEach(client -> {System.out.println(client);});
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SalesApp.class, args);
--- src/main/java/pedromachakio/com/github/domain/entity/Client.java
+++ src/main/java/pedromachakio/com/github/domain/entity/Client.java
@@ -5,6 +5,18 @@ public class Client {
     private Integer id;
     private String name;
 
+    public Client() {
+    }
+
+    public Client(String name) {
+        this.name = name;
+    }
+
+    public Client(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Integer getId() {
         return id;
     }
@@ -19,5 +31,13 @@ public class Client {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                " id = " + id +
+                ", name = '" + name + '\'' +
+                '}';
     }
 }
