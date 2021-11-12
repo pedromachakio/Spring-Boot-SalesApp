1bfd76616f1abf4689dbeb70ad0d2badf84c58d8
pedromachakio <pedromachakio@hotmail.com>
2021-11-12 11:01:33 -0100

    NOTES - pushing some notes regarding RequestMapping params before deleting them. Also deleting all the notes/comments throughout the app to clean it up

--- src/main/java/pedromachakio/com/github/rest/controller/ClientController.java
+++ src/main/java/pedromachakio/com/github/rest/controller/ClientController.java
@@ -10,7 +10,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/api/clients")
 public class ClientController {
 
-    @RequestMapping(value = "/hello/{name}", method = RequestMethod.GET)
+    @RequestMapping(
+            value = {"/hello/{name}", "/greetings/{name}"},  // o mapping pode ter vários paths
+            method = RequestMethod.GET
+            // consumes {"application/json", "application/xml"} se tivesse um RequestBody nos args do método para o utilizador fornecer json ou xml
+            // produces {"application/json", "application/xml"} se fosse para devolver json ou xml (json é default e implicit)
+    )
     @ResponseBody // diz que o retorno do método será o body da HTTP response
     public String helloClient(@PathVariable("name") String clientName) {
         return String.format("Hello %s ", clientName); // new java 5+ string interpolation
