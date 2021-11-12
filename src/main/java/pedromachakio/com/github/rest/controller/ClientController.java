b7086b26cb49af779c9a8c68de76dbca7557bb4e
pedromachakio <pedromachakio@hotmail.com>
2021-11-12 15:44:31 -0100

    NOTES - deleted all notes & refactored the CRUD methods and the ClientController to be a RestController.

--- src/main/java/pedromachakio/com/github/rest/controller/ClientController.java
+++ src/main/java/pedromachakio/com/github/rest/controller/ClientController.java
@@ -2,83 +2,77 @@ package pedromachakio.com.github.rest.controller;
 
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import pedromachakio.com.github.domain.entity.Client;
 import pedromachakio.com.github.domain.repository.ClientsDAO;
 
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 @RequestMapping("/api/clients")
 public class ClientController {
 
     private ClientsDAO clientsDAO;
 
-    public ClientController(ClientsDAO clientsDAO) { // dependency injection through constructor
+    public ClientController(ClientsDAO clientsDAO) {
         this.clientsDAO = clientsDAO;
     }
 
     @GetMapping("/{id}")
-    @ResponseBody
-    public ResponseEntity<Client> getClientById(@PathVariable Integer id) { // para dizer que esta var vai estar no actual link/path
-        Optional<Client> clientOptional = clientsDAO.findById(id); // optional porque pode ou não existir um cliente com este id
+    public Client getClientById(@PathVariable Integer id) {
+        return clientsDAO
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Couldn't find client."));
 
-        if (clientOptional.isPresent()) {
-            return ResponseEntity.ok(clientOptional.get()); // para responder com 200 e com o respetivo cliente desse id
-        } else {
-            return ResponseEntity.notFound().build(); //404
-        }
     }
 
     @PostMapping
-    @ResponseBody
-    public ResponseEntity<Client> saveClient(@RequestBody Client client) { // request porque é o utilizador que fornece
-        Client savedClient = clientsDAO.save(client);
-        return ResponseEntity.ok(savedClient);
+    @ResponseStatus(HttpStatus.CREATED) // Convenção é retornar estado 201 em vez de 200 quando é criado com sucesso
+    public Client saveClient(@RequestBody Client client) {
+        return clientsDAO.save(client);
     }
 
     @DeleteMapping("/{id}")
-    @ResponseBody
-    public ResponseEntity<Client> deleteClient(@PathVariable Integer id) {
-        Optional<Client> clientOptional = clientsDAO.findById(id);
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteClient(@PathVariable Integer id) {
+        clientsDAO
+                .findById(id)
+                .map(clientToBeDeleted -> {
+                    clientsDAO.delete(clientToBeDeleted);
+                    return clientToBeDeleted;
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Couldn't find client."));
 
-        if (clientOptional.isPresent()) {
-            clientsDAO.delete(clientOptional.get());
-            return ResponseEntity.noContent().build();
-        } else {
-            return ResponseEntity.notFound().build(); //404
-        }
     }
 
     @PutMapping("/{id}")
-    @ResponseBody
-    public ResponseEntity updateClient(@PathVariable Integer id, @RequestBody Client providedClient) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateClient(@PathVariable Integer id, @RequestBody Client providedClient) {
 
-        return clientsDAO
+        clientsDAO
                 .findById(id)
-                .map(alreadyExistingClient -> { // if is present does this, otherwise return an empty Optional (check this map method)
+                .map(alreadyExistingClient -> {
                     providedClient.setId(alreadyExistingClient.getId());
-                    //client.setName(existingClient.getName()); só é preciso fazer para o ID o resto das props atualizados já estão no RequestBody e são feitas tmb
                     clientsDAO.save(providedClient);
-                    return ResponseEntity.noContent().build();
-                }).orElseGet(() -> ResponseEntity.notFound().build()
-                );
+                    return alreadyExistingClient;
+                }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Couldn't find client."));
+
     }
 
     @GetMapping
-    @ResponseBody
-    public ResponseEntity findClient(Client filterClient) {
+    public List<Client> findClient(Client filterClient) {
         ExampleMatcher exampleMatcher = ExampleMatcher
                 .matching()
-                .withIgnoreCase() // ignores upper and lower case, matches both
-                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING); // tem que conter a sequência de caracteres anywhere na palavra
+                .withIgnoreCase()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
 
-        Example<Client> example = Example.of(filterClient, exampleMatcher); // retrieves the properties that are not null and that match
+        Example<Client> example = Example.of(filterClient, exampleMatcher);
 
-        List<Client> clientList = clientsDAO.findAll(example);
-        return ResponseEntity.ok(clientList);
+        return clientsDAO.findAll(example);
+
     }
 }
