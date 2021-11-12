f02c227b5ac0ec4838f3ca05cf91a6cd916d7bc1
pedromachakio <pedromachakio@hotmail.com>
2021-11-12 11:37:46 -0100

    implemented the REST GET method in ClientController and a 404 if no client exists with that ID in the h2 db

--- src/main/java/pedromachakio/com/github/SalesApp.java
+++ src/main/java/pedromachakio/com/github/SalesApp.java
@@ -1,10 +1,23 @@
 package pedromachakio.com.github;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import pedromachakio.com.github.domain.entity.Client;
+import pedromachakio.com.github.domain.repository.ClientsDAO;
 
 @SpringBootApplication
 public class SalesApp {
+
+    @Bean
+    public CommandLineRunner commandLineRunner(@Autowired ClientsDAO clientsDAO) {
+        return args -> {
+            Client clientSergiy = new Client(null, "Sergiy");
+            clientsDAO.save(clientSergiy);
+        };
+    }
 
 
     public static void main(String[] args) {
--- src/main/java/pedromachakio/com/github/rest/controller/ClientController.java
+++ src/main/java/pedromachakio/com/github/rest/controller/ClientController.java
@@ -1,22 +1,32 @@
 package pedromachakio.com.github.rest.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import pedromachakio.com.github.domain.entity.Client;
+import pedromachakio.com.github.domain.repository.ClientsDAO;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/api/clients")
 public class ClientController {
 
-    @RequestMapping(
-            value = {"/hello/{name}", "/greetings/{name}"},
-            method = RequestMethod.GET
+    private ClientsDAO clientsDAO;
 
-    )
+    public ClientController(ClientsDAO clientsDAO) { // dependency injection through constructor
+        this.clientsDAO = clientsDAO;
+    }
+
+    @GetMapping("/{id}")
     @ResponseBody
-    public String helloClient(@PathVariable("name") String clientName) {
-        return String.format("Hello %s ", clientName);
+    public ResponseEntity<Client> getClientById(@PathVariable Integer id) { // para dizer que esta var vai estar no actual link/path
+        Optional<Client> clientOptional = clientsDAO.findById(id); // optional porque pode ou não existir um cliente com este id
+
+        if(clientOptional.isPresent()) {
+            return ResponseEntity.ok(clientOptional.get()); // para responder com 200 e com o respetivo cliente desse id
+        } else {
+            return ResponseEntity.notFound().build(); //404
+        }
     }
 }
