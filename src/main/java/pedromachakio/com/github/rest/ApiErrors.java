b265e437cfd5142b062f3ba9e5a7c3843bafe687
pedromachakio <pedromachakio@hotmail.com>
2021-11-15 13:47:17 -0100

    implemented @RestControllerAdvice and @ExceptionHandler for custom exception handling

---
+++ src/main/java/pedromachakio/com/github/rest/ApiErrors.java
@@ -0,0 +1,19 @@
+package pedromachakio.com.github.rest;
+
+import lombok.Data;
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class ApiErrors {
+
+    @Getter
+    private List<String> errors;
+
+    public ApiErrors(String errorMessage) {
+        this.errors = Arrays.asList(errorMessage);
+    }
+
+}
--- src/main/java/pedromachakio/com/github/rest/controller/ApplicationControllerAdvice.java
+++ src/main/java/pedromachakio/com/github/rest/controller/ApplicationControllerAdvice.java
@@ -1,7 +1,19 @@
 package pedromachakio.com.github.rest.controller;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import pedromachakio.com.github.exception.BusinessLogicException;
+import pedromachakio.com.github.rest.ApiErrors;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(BusinessLogicException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleBusinessLogicException(BusinessLogicException businessLogicException) {
+        String errorMessage = businessLogicException.getMessage();
+        return new ApiErrors(errorMessage);
+    }
 }
