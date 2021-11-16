aa1873271dad5a44ed46fba041a1a905d01ba092
pedromachakio <pedromachakio@hotmail.com>
2021-11-16 17:21:48 -0100

    implemented the AuthenticationManagerBuilder with an in memory user for testing purposes

--- src/main/java/pedromachakio/com/github/config/SecurityConfig.java
+++ src/main/java/pedromachakio/com/github/config/SecurityConfig.java
@@ -1,16 +1,31 @@
 package pedromachakio.com.github.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+    @Bean
+    public PasswordEncoder passwordEncoder() { // para encriptar e desencriptar as passes dos utilizadores
+        return new BCryptPasswordEncoder(); // gera sempre um hash random, logo é mais seguro
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception { // configurar autenticação
-        super.configure(auth);
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder())
+                .withUser("fulano")
+                .password(passwordEncoder().encode("123"))
+                .roles("USER");
+
+                // criando um utilizador em memória só para testar (não vem de base de dados nem nada)
     }
 
     @Override
