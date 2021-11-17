fb92d6eace04a6ab621dc33cf97d504d72b41dd9
pedromachakio <pedromachakio@hotmail.com>
2021-11-17 15:14:50 -0100

    changed the user that was being created during run time to a user being created with UserDetailsService interface implementation

--- src/main/java/pedromachakio/com/github/config/SecurityConfig.java
+++ src/main/java/pedromachakio/com/github/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package pedromachakio.com.github.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,10 +8,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import pedromachakio.com.github.services.impl.UserServiceImpl;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserServiceImpl userService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -19,11 +23,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder())
-                .withUser("fulano")
-                .password(passwordEncoder().encode("123"))
-                .roles("USER", "ADMIN");
+        auth
+                .userDetailsService(userService)
+                .passwordEncoder(passwordEncoder());
 
 
     }
---
+++ src/main/java/pedromachakio/com/github/services/impl/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package pedromachakio.com.github.services.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        if(!username.equals("broski")) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return User
+                .builder()
+                .username("broski")
+                .password(passwordEncoder.encode("123"))
+                .roles("USER", "ADMIN")
+                .build();
+    }
+}
