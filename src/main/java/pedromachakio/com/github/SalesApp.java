c69530fb038d317d5c74ff8ada08b8bac23824bd
Pedro Batista <pedromachakio@hotmail.com>
2021-11-21 17:26:57 -0100

    NOTES - check on how to build. Added dependency and packaging to be able to package as WAR file

--- src/main/java/pedromachakio/com/github/SalesApp.java
+++ src/main/java/pedromachakio/com/github/SalesApp.java
@@ -2,12 +2,21 @@ package pedromachakio.com.github;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SalesApp {
+public class SalesApp extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SalesApp.class, args);
+
+        // mvn clean package to build the JAR file
+        // java -jar nomeDoFicheiro.jar para correr a packaged app
+
+        // add war to <packaging> in pom to package as a war file instead of jar and also respective dependency, starter-tomcat
+        // e extends SpringBootServletInitializer na main class para se tornar web app que gera war
+        // comandos are the same:
+        // mvn clean package
 
     }
 }
